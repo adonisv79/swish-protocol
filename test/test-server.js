a72c00afb217e64569fdcba0d99b407335307cfe
adonisvillamor <adonisv79@gmail.com>
2019-06-30 12:18:00 +0800

    added initial docs and changed the test-server to utilize our adon-api-session.SessionManageBasic library

--- test/test-server.js
+++ test/test-server.js
@@ -1,23 +1,12 @@
+//lets start with a basic session handler.
+const { SessionManagerBasic } = require('adon-api-session');
+const session = new SessionManagerBasic();
 //lets setup our web server sample using express
 const port = 3000
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 app.use(bodyParser.json())
-
-//in this sample, lets store the sessions in memory
-const sessions = {};
-//sample code for creating a new session id in memory
-function createNewSessionID() {
-	const new_session_id = Math.random()*100000000000000000;
-	if (sessions[new_session_id]) {
-		return createNewSessionID();
-	} else {
-		sessions[new_session_id] = {}
-		return new_session_id;
-	}
-}
-/****************************************/
 
 //now lets make use of the handshak library
 const HandshakeServer = require('../index.js').HandshakeServer;
@@ -33,13 +22,13 @@ app.post('/' + auth.path, (req, res) => {
 	console.dir(req.body)
 	//generate a unique session id with the client public key, store this in memory or a memory storage like redis/memcache
 	//the response_pub_key is used to encrypt the service's responses moving forward
-	const session_id = createNewSessionID();	
+	const session_id = session.createSession({});	
 	const result = auth.handleHandshakeRequest(req.headers, req.body, session_id);
-	sessions[session_id].response = {}
-	sessions[session_id].response.pub_key = result.session_keys.response_pub_key;
+	session.find(session_id).response = {}
+	session.find(session_id).response.pub_key = result.session_keys.response_pub_key;
 	
 	//store the request_decryption private key in the session
-	sessions[session_id].request = result.session_keys.request
+	session.find(session_id).request = result.session_keys.request
 	console.log('***HANDSHAKE:RESPONDING***')
 	console.log('header:')
 	console.dir(result.headers)
@@ -59,11 +48,11 @@ app.post('/test', (req, res) => {
 	console.log('body:')
 	console.dir(req.body)
 	//retrieve the private key and passphrase for the session and decryptRequest
-	if (!sessions[req.headers.swhs_sess_id]) {
+	if (!session.find(req.headers.swhs_sess_id)) {
 		res.send(403, 'Invalid Session');
 	}
-	const private_key = sessions[req.headers.swhs_sess_id].request.private_key;
-	const passphrase = sessions[req.headers.swhs_sess_id].request.created_date;
+	const private_key = session.find(req.headers.swhs_sess_id).request.private_key;
+	const passphrase = session.find(req.headers.swhs_sess_id).request.created_date;
 	//decrypt the body with the decryptRequest function
 	const req_body = auth.decryptRequest(private_key, passphrase, {
 		SWHS_ALGORITHM: req.headers.swhs_algorithm,
@@ -84,8 +73,7 @@ app.post('/test', (req, res) => {
 		response = {secret_response: 'Unknown action, should be hello or move'}
 	}
 	//encrypt the message before sending
-	const response_pub_key = sessions[req.headers.swhs_sess_id].response.pub_key;
-	console.dir(sessions)
+	const response_pub_key = session.find(req.headers.swhs_sess_id).response.pub_key;
 	response = auth.encryptResponse(req.headers.swhs_sess_id, response_pub_key, response)
 	console.log('***RESPONDED***')
 	console.dir(response)

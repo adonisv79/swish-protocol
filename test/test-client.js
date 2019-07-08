47b08f11cb174ccab42d158e7458dc29ed12f0dd
adonisvillamor <adonisv79@gmail.com>
2019-07-08 23:07:54 +0800

    added SWHS_ACTION, SWHS_NEXT to headers. enc_body will no longer contain any SWHS parts like pub keys. added common validateSwhsHeader check function added some fix to the test modules

--- test/test-client.js
+++ test/test-client.js
@@ -1,8 +1,8 @@
 const rp = require('request-promise');
 const HandshakeClient = require('../index.js').HandshakeClient;
-const SERVER_PROTOCOL = 'http'
-const SERVER_IP = '127.0.0.1'
-const SERVER_PORT = '3000'
+const SERVER_PROTOCOL = 'http';
+const SERVER_IP = '127.0.0.1';
+const SERVER_PORT = '3000';
 
 const hs_client = new HandshakeClient({
 	rsa_modulus_length: 512,
@@ -14,9 +14,9 @@ async function test () {
 	hs_client.resetKeys(); //creates a new handshake key pairing
 	try{
 		let swhs = hs_client.generateHandshake();
-		console.log('********************************************************************')
-		console.log('***HANDSHAKE:INITIATING***')
-		console.dir(swhs)
+		console.log('********************************************************************');
+		console.log('***HANDSHAKE:INITIATING***');
+		console.dir(swhs);
 		let result = await rp({
 			headers: swhs.headers,
 			method: 'POST',
@@ -24,31 +24,24 @@ async function test () {
 			uri: hs_client.uri_auth,
 			body: swhs.body,
 			resolveWithFullResponse: true
-		})
-		console.log('***HANDSHAKE:RECEIVED***')
-		console.dir(result.body)
-		const handshake_pairing = {
-			SWHS_SESS_ID: result.headers.swhs_sess_id,
-			SWHS_ALGORITHM: result.headers.swhs_algorithm,
-			SWHS_KEY: result.headers.swhs_key,
-			SWHS_IV: result.headers.swhs_iv,
-			is_json: result.body.is_json,
-			enc_body: result.body.enc_body
-		}
-		var data = hs_client.handleHandshakeResponse(handshake_pairing)
-		console.log('***HANDSHAKE:PAIRED***')
-		console.dir(data.request)
-		console.log('********************************************************************')
-		console.log('')
+		});
+		console.log('***HANDSHAKE:RECEIVED***');
+        console.dir(result.headers);
+        console.dir(result.body);
+		var data = hs_client.handleHandshakeResponse(result.headers, result.body);
+		console.log('***HANDSHAKE:PAIRED***');
+		console.dir(data.request);
+		console.log('********************************************************************');
+		console.log('');
 		
 		//now lets start communicating to the secured endpoints
 		swhs = hs_client.encryptRequest({
 			message: "Adonis Villamor",
 			passcode: 'whoami',
 			action: 'hello'
-		})
-		console.log('***SENDING***')
-		console.dir(swhs)
+		});
+		console.log('***SENDING***');
+		console.dir(swhs);
 		result = await rp({ 
 			headers: swhs.headers, 
 			method: 'POST', 
@@ -56,30 +49,30 @@ async function test () {
 			uri: hs_client.uri + 'test', 
 			body: swhs.body,
 			resolveWithFullResponse: true
-			})
-		console.log('***RECEIVED***')
-		console.dir(result.headers)
-		console.dir(result.body)
+			});
+		console.log('***RECEIVED***');
+		console.dir(result.headers);
+		console.dir(result.body);
 		result = hs_client.decryptResponse({
 			SWHS_ALGORITHM: result.headers.swhs_algorithm,
 			SWHS_KEY: result.headers.swhs_key,
 			SWHS_IV: result.headers.swhs_iv,
 			is_json: result.body.is_json,
 			enc_body: result.body.enc_body
-		})
-		console.log('***DECRYPTED***')
+		});
+		console.log('***DECRYPTED***');
 		console.dir(result);
-		console.log('********************************************************************')
-		console.log('')
+		console.log('********************************************************************');
+		console.log('');
 		
 		//send a different one this time
 		swhs = hs_client.encryptRequest({
 			message: "Japan",
 			passcode: 'whereami',
 			action: 'move'
-		})
-		console.log('***SENDING***')
-		console.dir(swhs)
+		});
+		console.log('***SENDING***');
+		console.dir(swhs);
 		result = await rp({ 
 			headers: swhs.headers, 
 			method: 'POST', 
@@ -87,23 +80,23 @@ async function test () {
 			uri: hs_client.uri + 'test', 
 			body: swhs.body,
 			resolveWithFullResponse: true
-			})
-		console.log('***RECEIVED***')
-		console.dir(result.headers)
-		console.dir(result.body)
+			});
+		console.log('***RECEIVED***');
+		console.dir(result.headers);
+		console.dir(result.body);
 		result = hs_client.decryptResponse({
 			SWHS_ALGORITHM: result.headers.swhs_algorithm,
 			SWHS_KEY: result.headers.swhs_key,
 			SWHS_IV: result.headers.swhs_iv,
 			is_json: result.body.is_json,
 			enc_body: result.body.enc_body
-		})
-		console.log('***DECRYPTED***')
+		});
+		console.log('***DECRYPTED***');
 		console.dir(result);
-		console.log('********************************************************************')
+		console.log('********************************************************************');
 		console.log('')
 	} catch (err) {
-		console.error('failed');
+		console.error(err);
 	}
 }
 
--- test/test-server.js
+++ test/test-server.js
@@ -2,11 +2,11 @@
 const { SessionManagerBasic } = require('adon-api-session');
 const session = new SessionManagerBasic();
 //lets setup our web server sample using express
-const port = 3000
-const express = require('express')
+const port = 3000;
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
 //now lets make use of the handshak library
 const HandshakeServer = require('../index.js').HandshakeServer;
@@ -14,56 +14,48 @@ const auth = new HandshakeServer();
 
 //create an endpoint listening to the recommended authentication path
 app.post('/' + auth.path, (req, res) => {
-	console.log('********************************************************************')
-	console.log('***HANDSHAKE:REQUESTED***')
-	console.log('header:')
-	console.dir(req.headers)
-	console.log('body:')
-	console.dir(req.body)
-	//generate a unique session id with the client public key, store this in memory or a memory storage like redis/memcache
-	//the response_pub_key is used to encrypt the service's responses moving forward
+	console.log('********************************************************************');
+	console.log('***HANDSHAKE:REQUESTED***');
+	console.log('header:');
+	console.dir(req.headers);
+	console.log('body:');
+	console.dir(req.body);
+	//generate a unique session id using a session manager
 	const session_id = session.createSession({});	
-	const result = auth.handleHandshakeRequest(req.headers, req.body, session_id);
-	session.find(session_id).response = {}
-	session.find(session_id).response.pub_key = result.session_keys.response_pub_key;
-	
-	//store the request_decryption private key in the session
-	session.find(session_id).request = result.session_keys.request
-	console.log('***HANDSHAKE:RESPONDING***')
-	console.log('header:')
-	console.dir(result.headers)
-	console.log('body:')
-	console.dir(result.body)
-	console.log('********************************************************************')
-	console.log('')
-	res.set(result.headers)
-	res.send(result.body) 
+	const result = auth.handleHandshakeRequest(req.headers, session_id);
+
+	//store the next request decryption items in the session
+	session.find(session_id).decrypt = result.decrypt;
+
+	console.log('***HANDSHAKE:RESPONDING***');
+	console.log('header:');
+	console.dir(result.headers);
+	console.log('body:');
+	console.dir(result.body);
+	console.log('********************************************************************');
+	console.log('');
+	res.set(result.headers);
+	res.send(result.body);
 });
 
 //create a test endpoint to see if data are transfered securely
 app.post('/test', (req, res) => {
-	console.log('***RECEIVED***')
-	console.log('header:')
-	console.dir(req.headers)
-	console.log('body:')
-	console.dir(req.body)
+	console.log('***RECEIVED***');
+	console.log('header:');
+	console.dir(req.headers);
+	console.log('body:');
+	console.dir(req.body);
 	//retrieve the private key and passphrase for the session and decryptRequest
 	if (!session.find(req.headers.swhs_sess_id)) {
 		res.send(403, 'Invalid Session');
 	}
-	const private_key = session.find(req.headers.swhs_sess_id).request.private_key;
-	const passphrase = session.find(req.headers.swhs_sess_id).request.created_date;
+	const private_key = session.find(req.headers.swhs_sess_id).decrypt.next_prv;
+	const passphrase = session.find(req.headers.swhs_sess_id).decrypt.created_date;
 	//decrypt the body with the decryptRequest function
-	const req_body = auth.decryptRequest(private_key, passphrase, {
-		SWHS_ALGORITHM: req.headers.swhs_algorithm,
-		SWHS_KEY: req.headers.swhs_key,
-		SWHS_IV: req.headers.swhs_iv, 
-		is_json: req.body.is_json,
-		enc_body: req.body.enc_body
-	})
+	const req_body = auth.decryptRequest(private_key, passphrase, req.headers, req.body);
 	
-	console.log('***DECRYPTED BODY***')
-	console.dir(req_body)
+	console.log('***DECRYPTED BODY***');
+	console.dir(req_body);
 	let response;
 	if (req_body.action == 'hello' && req_body.passcode== 'whoami') {
 		response = {secret_response: 'Hello ' + req_body.message}
@@ -73,14 +65,13 @@ app.post('/test', (req, res) => {
 		response = {secret_response: 'Unknown action, should be hello or move'}
 	}
 	//encrypt the message before sending
-	const response_pub_key = session.find(req.headers.swhs_sess_id).response.pub_key;
-	response = auth.encryptResponse(req.headers.swhs_sess_id, response_pub_key, response)
-	console.log('***RESPONDED***')
-	console.dir(response)
-	console.log('********************************************************************')
-	console.log('')
-	res.set(response.headers)
+	response = auth.encryptResponse(req.headers.swhs_sess_id, response);
+	console.log('***RESPONDED***');
+	console.dir(response);
+	console.log('********************************************************************');
+	console.log('');
+	res.set(response.headers);
 	return res.send(response.body)
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
